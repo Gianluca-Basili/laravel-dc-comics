c1ef0a10877ed33eab178daa0c464217e0beda95
Gianluca-Basili <gianluc.basili@gmail.com>
2023-08-02 19:06:14 +0200

    add new file index and add php artisan -rcms

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,12 @@
     <div class="container">
         <div class="row">
             <div class="col-12 mt-5">
-                <h1 class="text-center">BoolMovies</h1>
+                <h1>DC COMICS</h1>
             </div>
             <div class="col-12 mt-4">
-                <div class="d-flex justify-content-center">
-                    <a href="#" class="btn btn-primary mx-2">Visualizza film</a>
-                    <a href="#" class="btn btn-primary mx-2">Aggiungi film</a>
+                <div>
+                    <a href="#" class="btn btn-primary mx-2">Visualizza tutti i fumetti</a>
+                    
                 </div>
             </div>
         </div>
